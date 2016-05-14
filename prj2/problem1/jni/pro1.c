bf42080575d9e9bb0e17eccbb798af714b388441
GinYM <565953583@qq.com>
2016-05-14 18:10:38 +0800

    update pro1 requirement 1 to 3

--- prj2/problem1/jni/pro1.c
+++ prj2/problem1/jni/pro1.c
@@ -30,10 +30,10 @@ int isEqual(char *ch1,char *ch2)
 }
 
 
-int findTestID(struct prinfo* tmp,int size)
+int findTestID(struct prinfo* tmp,int size,char*test_name)
 {
 	int i;
-	char test_name[17] = "est.processtest";
+//	char test_name[17] = "est.processtest";
 	for(i=0;i<size;i++)
 	{
 		if(isEqual(tmp[i].comm,test_name) == 1)
@@ -43,6 +43,8 @@ int findTestID(struct prinfo* tmp,int size)
 	}
 	return -1;
 }
+
+
 
 
 int main(int argc,char *argv[])
@@ -55,34 +57,34 @@ int main(int argc,char *argv[])
 
 
 	struct sched_param param;
+    struct sched_param param1;
+    struct sched_param param2;
 	int maxpri_FIFO,maxpri_NORMAL,maxpri_RR;
 	maxpri_FIFO = sched_get_priority_max(SCHED_FIFO);
 	maxpri_NORMAL = sched_get_priority_max(SCHED_NORMAL);
 	maxpri_RR = sched_get_priority_max(SCHED_RR);
+    int priority;
 	int current_type;
-	if(maxpri_NORMAL == -1 || maxpri_FIFO == -1 || maxpri_RR == -1)
-	{
-		perror("sched_get_priority_max() failed!\n");
-	}
-	int pid;
+	int pid,pid1;
 	int type;
-	pid = findTestID(store,size);
+    char test_name[17] = "est.processtest";
+	pid = findTestID(store,size,test_name);
+    char test_name1[17] = "pro1TestARM";
+    pid1 = findTestID(store,size,test_name1);
 	if(pid == -1)
 	{
 		printf("Error! Didn't start the app!\n");
 		return 0;
 	}
+    if(pid1 == -1)
+    {
+        printf("Error! Didn't start the test code!\n");
+        return 0;
+    }
 	printf("the test pid of test app is: %d\n",pid);
 //	printf("the name is :%s\n",store[pid].comm);
 	for(;;)      //1,2 requirement
 	{
-	//	printf("Please input the pid(-1 to quit):");
-	//	scanf("%d",&pid);
-	//	if(pid == -1)
-	//	{
-	//		break;
-	//	}
-
 		current_type = sched_getscheduler(pid);
 		printf("Current scheduler policy: %d\n",current_type);
 		printf("Please input the priority type\n");
@@ -90,8 +92,9 @@ int main(int argc,char *argv[])
 		scanf("%d",&type);
 		if(type ==0 )
 		{
+            
 			param.sched_priority=maxpri_NORMAL;
-			if(sched_setscheduler(pid,SCHED_NORMAL,&param) ==-1)
+			if(sched_setscheduler(pid,SCHED_NORMAL,&param) ==-1 || sched_setscheduler(pid1,SCHED_NORMAL,&param) ==-1 )
 			{
 				perror("sched_setscheduler() failed!\n");
 			}
@@ -99,8 +102,15 @@ int main(int argc,char *argv[])
 		}
 		else if(type == 1)
 		{
-			param.sched_priority=maxpri_FIFO;
-			if(sched_setscheduler(pid,SCHED_FIFO,&param) ==-1)
+            printf("Please input the priority for FIFO(0 to 99): ");
+            scanf("%d",&priority);
+            while(priority<0 || priority > 99)
+            {
+                printf("Please reinput the priority for FIFO(0 to 99)");
+                scanf("%d",&priority);
+            }
+			param.sched_priority=priority;
+			if(sched_setscheduler(pid,SCHED_FIFO,&param) ==-1 || sched_setscheduler(pid1,SCHED_FIFO,&param) ==-1)
 			{
 				perror("sched_setscheduler() failed!\n");
 			}
@@ -108,8 +118,15 @@ int main(int argc,char *argv[])
 		}
 		else if(type == 2)
 		{
-			param.sched_priority=maxpri_RR;
-			if(sched_setscheduler(pid,SCHED_RR,&param) ==-1)
+            printf("Please input the priority for RR(0 to 99): ");
+            scanf("%d",&priority);
+            while(priority<0 || priority > 99)
+            {
+                printf("Please reinput the priority for RR(0 to 99): ");
+                scanf("%d",&priority);
+            }
+			param.sched_priority=priority;
+			if(sched_setscheduler(pid,SCHED_RR,&param) ==-1|| sched_setscheduler(pid1,SCHED_RR,&param) ==-1)
 			{
 				perror("sched_setscheduler() failed!\n");
 			}
@@ -119,7 +136,7 @@ int main(int argc,char *argv[])
 			printf("finished requirement 1,2\n");
 			break;
 		}
-		printf("New policy: %d\n",sched_getscheduler(pid));
+		printf("New policy of app and test:%d %d\n",sched_getscheduler(pid),sched_getscheduler(pid1));
 	}
 
 //requirement 3
@@ -128,6 +145,7 @@ int main(int argc,char *argv[])
 	char ch_tmp[6] = "main";
 	int pid_tmp;
 	int type_3;
+    int pri3,pri1,pri2;
 	for(;;)
 	{
 		printf("Please input the type for requirement 3(0:NORMAL 1:RR -1:quit): ");
@@ -135,24 +153,32 @@ int main(int argc,char *argv[])
 		pid_tmp=-1;
 		if(type_3 == 1)
 		{
-			param.sched_priority=maxpri_RR-1;
+            do
+            {
+               printf("Please input the priority for process and test:\n");
+               scanf("%d%d",&pri1,&pri2);
+               pri3 = pri2;
+            }while(pri1<0 || pri1 > 99 || pri2<0 || pri2 > 99 || pri3 <0 || pri3 > 99 );
 		}
 		else if(type_3 == 0)
 		{
-			param.sched_priority=maxpri_NORMAL;
+			pri1=maxpri_NORMAL;
+            pri2 = maxpri_NORMAL;
+            pri3 = maxpri_NORMAL;
 		}
 		else
 		{
 			break;
 		}
+        param.sched_priority = pri1;
+        param1.sched_priority=pri2;
+        param2.sched_priority=pri3;
 		for(i=0;i<size;i++)
 		{
-			//printf("here!\n");
 			if(pid_tmp == -1 && isEqual(store[i].comm,ch_tmp) == 1)
 			{
-			//	printf("here\n");
 				pid_tmp=store[i].parent_pid;
-				printf("find the string:%s\n",store[i].comm);
+				printf("find the main:%s\n",store[i].comm);
 				continue;
 			}
 			if(pid_tmp != -1)
@@ -183,24 +209,20 @@ int main(int argc,char *argv[])
 			}
 		}
 		if(type_3 == 1)
-			param.sched_priority=maxpri_RR;
-		else
-			param.sched_priority=maxpri_NORMAL;
-		if(type_3 == 1)
 		{
-			if(sched_setscheduler(pid,SCHED_RR,&param) ==-1)
+			if(sched_setscheduler(pid,SCHED_RR,&param1) ==-1 || sched_setscheduler(pid1,SCHED_RR,&param2) ==-1)
 			{
 				perror("sched_setscheduler() failed!\n");
 			}
-			printf("test app is changed to RR\n");
+			printf("New policy of app and test:%d %d\n",sched_getscheduler(pid),sched_getscheduler(pid1));
 		}
 		else
 		{
-			if(sched_setscheduler(pid,SCHED_NORMAL,&param) ==-1)
+			if(sched_setscheduler(pid,SCHED_NORMAL,&param1) ==-1 || sched_setscheduler(pid1,SCHED_NORMAL,&param2) ==-1)
 			{
 				perror("sched_setscheduler() failed!\n");
 			}
-			printf("test app is changed to NORMAL\n");
+			printf("New policy of app and test:%d %d\n",sched_getscheduler(pid),sched_getscheduler(pid1));
 
 		}
 	}	
